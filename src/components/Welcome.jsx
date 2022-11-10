70c5329294a12adb6c7883b7afc49cc89f2f91b9
akashkumar27 <117928680+akashkumar27@users.noreply.github.com>
2022-11-10 21:33:00 +0530

    Add files via upload
    Initial Commit

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,31 @@
+import React from 'react' 
+import styled from 'styled-components'
+import Robot from "../assets/robot_hello.gif"
+export default function Welcome({currentUser}) {
+  return (
+    <Container>
+        <img src={Robot} alt="Robot" />
+        <h1>
+            Welcome, <span>{currentUser.username}!</span>
+        </h1>
+        <h3>
+            Please select a chat to start messaging
+        </h3>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: white;
+  img{
+    height: 10rem;
+    color: white;
+  }
+  span{
+    color: #4e00ff;
+  }
+`
